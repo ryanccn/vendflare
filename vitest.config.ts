1d75371d089f2b4bc069c2b184c5fc7d03e3d4c6
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-08-20 16:45:14 +0800

    chore: bump deps and remove miniflare environment

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,7 +7,6 @@ const projectRoot = dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
 	test: {
 		root: "./test",
-		environment: "miniflare",
 		alias: {
 			"@src": join(projectRoot, "src"),
 			"@dist": join(projectRoot, "dist"),
