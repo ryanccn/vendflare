f7e39d529fa93104f9926e3f6a5cca6c203386a6
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2024-06-24 11:38:25 +0800

    chore: update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -3,4 +3,7 @@ import { config } from '@ryanccn/eslint-config';
 export default config({
 	ignores: ['**/dist', '**/.wrangler', '**/test/coverage'],
 	stylistic: true,
+	rules: {
+		'unicorn/import-style': 'off',
+	},
 });
