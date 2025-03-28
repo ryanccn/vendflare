97c58666238ee127d4a02672bcbde5f638f9a4e6
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2025-03-28 23:29:23 +0800

    fix: manage tsconfigs properly

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import { config } from '@ryanccn/eslint-config';
 
 export default config({
-	ignores: ['**/dist', '**/.wrangler', '**/test/coverage'],
+	ignores: ['**/dist', '**/.wrangler', '**/coverage'],
 	stylistic: true,
 	rules: {
 		'unicorn/import-style': 'off',
